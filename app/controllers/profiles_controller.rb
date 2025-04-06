1f86b76fcf0b17b18eb4a5be31b8b119a1b6fd8a
Hayato-Kossy <kshyt2501@icloud.com>
2025-04-06 16:29:46 +0900

    hoge

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
    def index
     @user = User.find(1)
-    raise Forbidden unless user_safe?
+    raise Forbidden unless tuned_test_user_safe?
 
     @skill_categories = tuned_user_reccomend_skill_categories
     @articles = @user.articles.preload(:tags)
@@ -49,5 +49,20 @@ class ProfilesController < ApplicationController
       end
     end
     show_queries
+  end
+
+  def test_user_safe?
+    reset_queries
+    @user.user_cautions.all? do |user_caution|
+      Time.zone.now > user_caution.caution_freeze.end_time
+    end
+    show_queries
+  end
+
+  def tuned_test_user_safe?
+    # reset_queries
+    @user.user_cautions.joins(:caution_freeze).
+      where('caution_freezes.end_time > ?', Time.zone.now).empty?
+    # show_queries
   end
 end
